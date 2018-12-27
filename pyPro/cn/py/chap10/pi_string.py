8071ee62d3536f03c9fcf447cd500ca2c1e89b9d
Jon Loeliger <test@vmware.com>
2018-12-27 15:50:06 +0800

    chapter10

---
+++ pyPro/cn/py/chap10/pi_string.py
@@ -0,0 +1,28 @@
+# #使用读取到的文件内容
+# filename = "pi_digits.txt"
+#
+# with open(filename) as file_object:
+#     lines = file_object.readlines()
+#
+# pi_string = ''
+# for line in lines:
+#     pi_string += line.strip()
+#
+# print(pi_string)
+# print(len(pi_string))
+
+# #另一个例子
+filename = "pi_million_digits.txt"
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+
+birthday = input("Enter your birthday, in the form mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday appears in the first million digits of pi!")
+else:
+    print("Your birthday does not appear in the first million digits of pi.")
---
+++ pyPro/cn/py/chap10/write_message.py
@@ -0,0 +1,11 @@
+filename = "programming.txt"
+
+# 写模式打开文件
+with open(filename, 'w') as file_object:
+    file_object.write("I love programming.\n")
+    file_object.write("I love creating new games.\n")
+
+# 追加形式打开文件
+with open(filename, 'a') as file_object:
+    file_object.write("I also love finding meaning in large datasets.\n")
+    file_object.write("I love creating apps that can run in a brower.\n")

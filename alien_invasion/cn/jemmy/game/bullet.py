dcf17dd8a343998498f1d69b850a478f493a1696
Jon Loeliger <test@vmware.com>
2019-01-02 13:05:51 +0800

    chapter13

--- alien_invasion/cn/jemmy/game/bullet.py
+++ alien_invasion/cn/jemmy/game/bullet.py
@@ -11,6 +11,7 @@ class Bullet(Sprite):
         self.screen = screen
 
         # 在(0, 0)创建一个表示子弹的矩形,再设置正确的位置
+        #self.rect = pygame.draw.circle(screen,[255, 69, 0], ship.rect.top, 10, 0)
         self.rect = pygame.Rect(0, 0, ai_settings.bullet_width, ai_settings.bullet_height)
         self.rect.centerx = ship.rect.centerx
         self.rect.top = ship.rect.top

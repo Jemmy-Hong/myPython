6bd30c0b6f9ec25a67d5c825e2933626b6c3b7a3
Jon Loeliger <test@vmware.com>
2019-01-02 18:20:38 +0800

    chapter14

--- alien_invasion/cn/jemmy/game/alien_invasion.py
+++ alien_invasion/cn/jemmy/game/alien_invasion.py
@@ -5,6 +5,8 @@ from cn.jemmy.game.setting import Settings
 from cn.jemmy.game.ship import Ship
 from cn.jemmy.game import game_functions as gf
 from cn.jemmy.game.game_stats import GameStats
+from cn.jemmy.game.button import Button
+from cn.jemmy.game.scoreboard import Scoreboard
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
@@ -13,8 +15,12 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # 创建一个用于存储游戏统计信息的实例
+    # 创建Play按钮
+    play_button = Button(ai_settings, screen, "Play")
+
+    # 创建一个用于存储游戏统计信息的实例, 并创建记分牌
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
 
     # 创建一艘飞船, 一个用于存储子弹的编组,
     ship = Ship(ai_settings,screen)
@@ -27,13 +33,13 @@ def run_game():
 
     #开始游戏的主循环
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
 
         if stats.game_active:
             ship.update()
-            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 run_game()
